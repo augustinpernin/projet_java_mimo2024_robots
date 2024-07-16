50cb2d123667ec414a220711d3fe54a448bd1a83
augustinpernin <augustinpernin@gmail.com>
2024-07-16 11:57:22 +0200

    Initial commit

---
+++ Atelier_de_robots/src/main/java/com/mimo2024/Atelier_de_robots/AtelierDeRobotsApplication.java
@@ -0,0 +1,13 @@
+package com.mimo2024.Atelier_de_robots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelierDeRobotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelierDeRobotsApplication.class, args);
+	}
+
+}
---
+++ Atelier_de_robots/src/test/java/com/mimo2024/Atelier_de_robots/AtelierDeRobotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimo2024.Atelier_de_robots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelierDeRobotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
